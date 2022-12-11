404d91a5d3cb4a392bbafd29e3db367420f5fb38
Lawrence137 <117739171+Lawrence137@users.noreply.github.com>
2022-12-11 12:35:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function isPalindrome(word) {
   // Write your algorithm here
-  word = word.toLowerCase();
   let reverseWord = "";
   for (let i = word.length - 1; i >= 0; i--) {
     reverseWord += word[i];}
@@ -10,10 +9,6 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
-
-  function isPalindrome(word)
-  # convert the given word to lowercase
-  word = word.toLowerCase()
 
   # create an empty string to store the reverse of the word
   reverseWord = ""
@@ -39,23 +34,6 @@ function isPalindrome(word) {
     Finally, it compares the original word with its reverse and returns true if they are equal and false otherwise.
 */
 
-describe("isPalindrome", () => {
-  it("returns true for 'abba'", () => {
-    expect(isPalindrome("abba")).to.be.true;
-  });
-  it("returns true for 'racecar'", () => {
-    expect(isPalindrome("racecar")).to.be.true;
-  });
-  it("returns true for 'a'", () => {
-    expect(isPalindrome("a")).to.be.true;
-  });
-  it("returns false for 'robot'", () => {
-    expect(isPalindrome("robot")).to.be.false;
-  });
-  it("returns false for 'ab'", () => {
-    expect(isPalindrome("ab")).to.be.false;
-  });
-});
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
